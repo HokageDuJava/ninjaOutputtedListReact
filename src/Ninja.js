88e1bc70c20a2fcf38dfbd61fa7aba48e1c4920e
Arzad <arzad.aboubacar@gmail.com>
2020-05-17 16:58:40 +0200

    list of ninjas upgrade

--- src/Ninja.js
+++ src/Ninja.js
@@ -1,20 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Ninja extends Component {
-  render() {
-    const { ninjas } = this.props;
-    const ninjasList = ninjas.map((ninja) => {
-      return (
-        <div key={ninja.id} className="ninja">
-          <h2>My Name is : {ninja.name}</h2>
-          <p>I come from : {ninja.village} village</p>
-          <p>And I'm {ninja.age} years old !</p>
-        </div>
-      );
-    });
+const Ninja = ({ ninjas }) => {
+  const ninjasList = ninjas.map((ninja) => {
+    return ninja.age >= 13 ? (
+      <div key={ninja.id} className="ninja">
+        <h2>My Name is : {ninja.name}</h2>
+        <p>I come from : {ninja.village} village</p>
+        <p>And I'm {ninja.age} years old !</p>
+      </div>
+    ) : (
+      <div>
+        <p>This child is way too young to be a real ninja !</p>
+      </div>
+    );
+  });
 
-    return <div>{ninjasList}</div>;
-  }
-}
+  return <div>{ninjasList}</div>;
+};
 
 export default Ninja;
